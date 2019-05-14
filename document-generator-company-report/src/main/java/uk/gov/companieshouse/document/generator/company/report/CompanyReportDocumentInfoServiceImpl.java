a8203b07ec509bdefe5d8aae5e889c51c1385029
Shaun Mathias <smathias@companieshouse.gov.uk>
2019-05-14 15:41:46 +0100

    Updated company report and restructured layout Added all calls to api

--- document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/CompanyReportDocumentInfoServiceImpl.java
+++ document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/CompanyReportDocumentInfoServiceImpl.java
@@ -1,19 +1,9 @@
 package uk.gov.companieshouse.document.generator.company.report;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import uk.gov.companieshouse.api.model.company.CompanyProfileApi;
-import uk.gov.companieshouse.api.model.filinghistory.FilingHistoryApi;
-import uk.gov.companieshouse.document.generator.company.report.exception.ServiceException;
-import uk.gov.companieshouse.document.generator.company.report.model.CompanyReport;
-import uk.gov.companieshouse.document.generator.company.report.service.CompanyService;
-import uk.gov.companieshouse.document.generator.company.report.service.FilingHistoryService;
+import uk.gov.companieshouse.document.generator.company.report.exception.HandlerException;
+import uk.gov.companieshouse.document.generator.company.report.handler.CompanyReportDataHandler;
 import uk.gov.companieshouse.document.generator.interfaces.DocumentInfoService;
 import uk.gov.companieshouse.document.generator.interfaces.exception.DocumentInfoException;
 import uk.gov.companieshouse.document.generator.interfaces.model.DocumentInfoRequest;
@@ -21,13 +11,8 @@ import uk.gov.companieshouse.document.generator.interfaces.model.DocumentInfoRes
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.UUID;
 
 @Service
 public class CompanyReportDocumentInfoServiceImpl implements DocumentInfoService {
@@ -37,10 +22,7 @@ public class CompanyReportDocumentInfoServiceImpl implements DocumentInfoService
     private static final Logger LOG = LoggerFactory.getLogger(MODULE_NAME_SPACE);
 
     @Autowired
-    private CompanyService companyService;
-
-    @Autowired
-    private FilingHistoryService filingHistoryService;
+    private CompanyReportDataHandler companyReportDataHandler;
 
     @Override
     public DocumentInfoResponse getDocumentInfo(DocumentInfoRequest documentInfoRequest) throws DocumentInfoException {
@@ -53,90 +35,12 @@ public class CompanyReportDocumentInfoServiceImpl implements DocumentInfoService
 
         LOG.infoContext(requestId,"Started getting document info for company profile", debugMap);
 
-        CompanyProfileApi companyProfileApi;
-        FilingHistoryApi filingHistoryApi = null;
-
-        String companyNumber = getCompanyNumberFromUri(resourceUri);
-
         try {
-           companyProfileApi = companyService.getCompanyProfile(companyNumber);
-        } catch (ServiceException se) {
-            throw new DocumentInfoException("error occurred obtaining the company profile", se);
+            return companyReportDataHandler.getCompanyReportData(resourceUri, requestId);
+        } catch (HandlerException he) {
+            LOG.errorContext(requestId, String.format("An error occurred when retrieving the data for the company report"), he, debugMap);
+            throw new DocumentInfoException("Failed to get company report data for resourceUri: "
+                + resourceUri);
         }
-
-        Iterator it = companyProfileApi.getLinks().entrySet().iterator();
-
-        while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry) it.next();
-
-            if(pair.getKey() == "filing_history") {
-
-                try {
-                    filingHistoryApi = filingHistoryService.getFilingHistory(companyNumber);
-                }  catch (ServiceException se) {
-                    throw new DocumentInfoException("error occurred obtaining the filing history", se);
-                }
-            } else if(pair.getKey() == "officers") {
-                LOG.info("officers was present. But, there is no implementation to obtain the officers in the SDK yet");
-            }
-        }
-
-        return createDocumentInfoResponse(companyProfileApi, filingHistoryApi);
-    }
-
-    private DocumentInfoResponse createDocumentInfoResponse(CompanyProfileApi companyProfileApi, FilingHistoryApi filingHistoryApi) throws DocumentInfoException {
-
-        DocumentInfoResponse documentInfoResponse = new DocumentInfoResponse();
-
-        documentInfoResponse.setData(createData(companyProfileApi, filingHistoryApi));
-        documentInfoResponse.setAssetId("accounts");
-        documentInfoResponse.setPath(createPathString());
-        documentInfoResponse.setTemplateName("company-report.html");
-
-        return documentInfoResponse;
-    }
-
-    private String createData(CompanyProfileApi companyProfileApi,
-                              FilingHistoryApi filingHistoryApi) throws DocumentInfoException {
-
-        CompanyReport companyReport = new CompanyReport();
-        companyReport.setCompanyProfileApi(companyProfileApi);
-        companyReport.setFilingHistoryApi(filingHistoryApi);
-
-        return toJson(companyReport);
-    }
-
-    private String toJson(CompanyReport companyReport) throws DocumentInfoException {
-
-        String reportToJson;
-        ObjectMapper mapper = new ObjectMapper();
-        JavaTimeModule javaTimeModule = new JavaTimeModule();
-        javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern("dd-MMMM-yyyy")));
-        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern("dd-MMMM-yyyy")));
-        mapper.registerModule(javaTimeModule);
-        mapper.setDateFormat(new SimpleDateFormat("dd-MMMM-yyyy"));
-
-        try {
-            reportToJson = mapper.writeValueAsString(companyReport);
-        } catch (JsonProcessingException e) {
-            throw new DocumentInfoException(
-                "Could not serialise Document Info for the company report for resource: "
-                    + companyReport.getCompanyProfileApi().getCompanyNumber());
-        }
-
-        return reportToJson;
-    }
-
-    private String createPathString() {
-        return String.format("/%s/%s", "accounts", getUniqueFileName());
-    }
-
-    public String getUniqueFileName() {
-        UUID uuid = UUID.randomUUID();
-        return "companyReport" + uuid.toString() + ".html";
-    }
-
-    private String getCompanyNumberFromUri(String resourceUri) {
-        return  resourceUri.replaceAll("[^\\d.]", "");
     }
 }
---
+++ document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/handler/CompanyReportDataHandler.java
@@ -0,0 +1,249 @@
+package uk.gov.companieshouse.document.generator.company.report.handler;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriTemplate;
+import uk.gov.companieshouse.api.ApiClient;
+import uk.gov.companieshouse.api.error.ApiErrorResponseException;
+import uk.gov.companieshouse.api.handler.exception.URIValidationException;
+import uk.gov.companieshouse.api.model.charges.ChargesApi;
+import uk.gov.companieshouse.api.model.company.CompanyProfileApi;
+import uk.gov.companieshouse.api.model.filinghistory.FilingHistoryApi;
+import uk.gov.companieshouse.api.model.insolvency.InsolvencyApi;
+import uk.gov.companieshouse.api.model.officers.OfficersApi;
+import uk.gov.companieshouse.api.model.psc.PscsApi;
+import uk.gov.companieshouse.document.generator.company.report.exception.HandlerException;
+import uk.gov.companieshouse.document.generator.company.report.exception.ServiceException;
+import uk.gov.companieshouse.document.generator.company.report.model.CompanyReport;
+import uk.gov.companieshouse.document.generator.company.report.model.items.FormattedAdditionalInformation;
+import uk.gov.companieshouse.document.generator.company.report.service.ApiClientService;
+import uk.gov.companieshouse.document.generator.company.report.service.CompanyService;
+import uk.gov.companieshouse.document.generator.interfaces.model.DocumentInfoResponse;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Component
+public class CompanyReportDataHandler {
+
+    @Autowired
+    private CompanyService companyService;
+
+    @Autowired
+    private ApiClientService apiClientService;
+
+    public static final String MODULE_NAME_SPACE = "document-generator-company-report";
+
+    private static final Logger LOG = LoggerFactory.getLogger(MODULE_NAME_SPACE);
+
+    private static final String NOT_FOUND_API_DATA = "No data found in %s api for link: ";
+
+    private static final UriTemplate GET_COMPANY_URI = new UriTemplate("/company/{companyNumber}");
+
+
+    /**
+     * Get Company Report data from the different resources associated with company number, and
+     * map the data for document generation
+     *
+     * @param resourceUri the resource uri of the company accounts
+     * @param requestId the id of the request
+     * @return a populated {@link DocumentInfoResponse} object
+     * @throws HandlerException throws a custom handler exception
+     */
+    public DocumentInfoResponse getCompanyReportData(String resourceUri, String requestId)
+        throws HandlerException {
+
+        String companyNumber = getCompanyNumberFromUri(resourceUri);
+
+        try {
+            LOG.infoContext(requestId, "Getting data for report for company number: " + companyNumber, getDebugMap(companyNumber));
+            return createDocumentInfoResponse(companyNumber);
+        } catch (URIValidationException | ApiErrorResponseException e) {
+            LOG.errorContext(requestId,"Failed to get data for report for company number " + companyNumber, e, getDebugMap(companyNumber));
+            throw new HandlerException(e.getMessage(), e.getCause());
+        }
+    }
+
+    private DocumentInfoResponse createDocumentInfoResponse(String companyNumber) throws HandlerException,
+        URIValidationException, ApiErrorResponseException {
+
+        DocumentInfoResponse documentInfoResponse = new DocumentInfoResponse();
+
+        documentInfoResponse.setData(getCompanyReportData(companyNumber));
+        documentInfoResponse.setAssetId("accounts");
+        documentInfoResponse.setPath(createPathString());
+        documentInfoResponse.setTemplateName("company-report.html");
+
+        return documentInfoResponse;
+    }
+
+    private String getCompanyReportData(String companyNumber) throws HandlerException,
+        URIValidationException, ApiErrorResponseException {
+
+        CompanyProfileApi companyProfileApi;
+        FilingHistoryApi filingHistoryApi = null;
+        InsolvencyApi insolvencyApi = null;
+        OfficersApi officersApi = null;
+        ChargesApi chargesApi = null;
+        PscsApi pscsApi = null;
+
+        companyProfileApi = getCompanyProfile(companyNumber);
+
+        ApiClient apiClient = apiClientService.getApiClient();
+
+        String errorString = "company report";
+
+        Iterator it = companyProfileApi.getLinks().entrySet().iterator();
+
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+
+            try {
+                if(pair.getKey() == "filing_history") {
+                    errorString = "filing history";
+                    filingHistoryApi = apiClient.filingHistory()
+                        .list(GET_COMPANY_URI.expand(companyNumber).toString() + "/filing-history").execute();
+                }
+
+                else if (pair.getKey() == "insolvency") {
+                    errorString ="insolvency";
+                    insolvencyApi = apiClient.insolvency()
+                        .get(GET_COMPANY_URI.expand(companyNumber).toString() + "/insolvency").execute();
+                }
+
+                else if(pair.getKey() == "officers") {
+                    errorString = "officers";
+                    officersApi = apiClient.officers()
+                        .get(GET_COMPANY_URI.expand(companyNumber).toString() + "/officers").execute();
+                }
+
+                else if(pair.getKey() == "charges") {
+                    errorString = "charges";
+                    chargesApi = apiClient.charges()
+                        .get(GET_COMPANY_URI.expand(companyNumber).toString() + "/charges").execute();
+                }
+
+                else if(pair.getKey() == "persons_with_significant_control_statements") {
+                    errorString = "persons with significant control statements";
+                    pscsApi = apiClient.pscs().list(GET_COMPANY_URI.expand(companyNumber).toString() +
+                            "/persons-with-significant-control-statements").execute();
+                }
+            } catch (ApiErrorResponseException e) {
+                handleException(e, errorString, companyNumber);
+            }
+        }
+
+        return mapCompanyReport(companyProfileApi, filingHistoryApi, insolvencyApi,
+            officersApi, chargesApi, pscsApi);
+    }
+
+    private CompanyProfileApi getCompanyProfile(String companyNumber) throws HandlerException {
+
+        try {
+            return companyService.getCompanyProfile(companyNumber);
+        } catch (ServiceException se) {
+            throw new HandlerException("error occurred obtaining the company profile", se);
+        }
+    }
+
+    private String mapCompanyReport(CompanyProfileApi companyProfileApi,
+        FilingHistoryApi filingHistoryApi,
+        InsolvencyApi insolvencyApi, OfficersApi officersApi,
+        ChargesApi chargesApi, PscsApi pscsApi) throws HandlerException {
+
+        CompanyReport companyReport = new CompanyReport();
+        companyReport.setCompanyProfile(companyProfileApi);
+        companyReport.setFilingHistory(filingHistoryApi);
+        companyReport.setInsolvency(insolvencyApi);
+        companyReport.setOfficers(officersApi);
+        companyReport.setCharges(chargesApi);
+        companyReport.setPscsApi(pscsApi);
+
+        companyReport.setFormattedAdditionalInformation(formatAdditionalInformation(companyProfileApi, filingHistoryApi));
+
+        return toJson(companyReport);
+    }
+
+    private FormattedAdditionalInformation formatAdditionalInformation(CompanyProfileApi companyProfileApi,
+        FilingHistoryApi filingHistoryApi) {
+
+        FormattedAdditionalInformation formattedAdditionalInformation = new FormattedAdditionalInformation();
+
+        formattedAdditionalInformation.setCompanyStatus("test");
+        formattedAdditionalInformation.setCompanyType("test");
+
+        List<String> natureOfBusinessSIC = new ArrayList<>();
+        for(String s : companyProfileApi.getSicCodes()) {
+            natureOfBusinessSIC.add("test");
+        }
+
+        return formattedAdditionalInformation;
+    }
+
+    private String toJson(CompanyReport companyReport) throws HandlerException {
+
+        String reportToJson;
+        ObjectMapper mapper = new ObjectMapper();
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern("dd-MMMM-yyyy")));
+        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern("dd-MMMM-yyyy")));
+        mapper.registerModule(javaTimeModule);
+        mapper.setDateFormat(new SimpleDateFormat("dd-MMMM-yyyy"));
+
+        try {
+            reportToJson = mapper.writeValueAsString(companyReport);
+        } catch (JsonProcessingException e) {
+            throw new HandlerException(
+                "Could not serialise Document Info for the company report for: "
+                    + companyReport.getCompanyProfile().getCompanyNumber() + ": "
+                    + companyReport.getCompanyProfile().getCompanyName());
+        }
+
+        return reportToJson;
+    }
+
+    private String getCompanyNumberFromUri(String resourceUri) {
+        return  resourceUri.replaceAll("[^\\d.]", "");
+    }
+
+    private String createPathString() {
+        return String.format("/%s/%s", "accounts", getUniqueFileName());
+    }
+
+    public String getUniqueFileName() {
+        UUID uuid = UUID.randomUUID();
+        return "companyReport" + uuid.toString() + ".html";
+    }
+
+    private void handleException(ApiErrorResponseException e, String text, String companyNumber)
+        throws ApiErrorResponseException {
+
+        if (e.getStatusCode() == HttpStatus.NOT_FOUND.value()) {
+            LOG.info(String.format(NOT_FOUND_API_DATA, text, companyNumber), getDebugMap(companyNumber));
+        } else {
+            throw e;
+        }
+    }
+
+    private Map<String, Object> getDebugMap(String companyNumber) {
+        Map<String, Object> logMap = new HashMap<>();
+        logMap.put("COMPANY_NUMBER", companyNumber);
+
+        return logMap;
+    }
+}
